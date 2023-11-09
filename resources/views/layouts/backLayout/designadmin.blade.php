34e454bba0d6c12a1849e588179d5ebdb31639e0
MOHAMED TRAORE <mo.traore@ldfgroupe.com>
2023-11-09 16:46:19 +0000

    mise a jour design pour processus

--- resources/views/layouts/backLayout/designadmin.blade.php
+++ resources/views/layouts/backLayout/designadmin.blade.php
@@ -52,16 +52,17 @@ $imagedashboard = Menu::get_info_image_dashboard();
     <!-- Core CSS -->
     <link rel="stylesheet" href="/assets/vendor/css/rtl/core.css"/>
     <link rel="stylesheet" href="/assets/vendor/css/rtl/theme-default.css"/>
-    <link rel="stylesheet" href="/assets/css/demo.css"/>
+    <link rel="stylesheet" href="/assets/css/demo.css"/>  
 
     <!-- Vendors CSS -->
-    <link rel="stylesheet" href="{{asset('backendvuexy/assets/vendor/libs/node-waves/node-waves.css')}}" />
-    <link rel="stylesheet" href="{{asset('backendvuexy/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')}}" />
-    <link rel="stylesheet" href="{{asset('backendvuexy/assets/vendor/libs/typeahead-js/typeahead.css')}}" />
-    <link rel="stylesheet" href="{{asset('backendvuexy/assets/vendor/libs/apex-charts/apex-charts.css')}}" />
-    <link rel="stylesheet" href="{{asset('backendvuexy/assets/vendor/libs/swiper/swiper.css')}}" />
-    <link rel="stylesheet" href="{{asset('backendvuexy/assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css')}}" />
-    <link rel="stylesheet" href="{{asset('backendvuexy/assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/vendor/libs/node-waves/node-waves.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/vendor/libs/typeahead-js/typeahead.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/vendor/libs/apex-charts/apex-charts.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/vendor/libs/swiper/swiper.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/vendor/libs/select2/select2.css')}}" />
 
     <!-- Page CSS -->
 
@@ -220,11 +221,13 @@ $imagedashboard = Menu::get_info_image_dashboard();
 <script src="{{asset('assets/vendor/libs/i18n/i18n.js')}}"></script>
 <script src="{{asset('assets/vendor/libs/typeahead-js/typeahead.js')}}"></script>
 <script src="{{asset('assets/vendor/js/menu.js')}}"></script>
+<script src="{{asset('assets/vendor/libs/select2/select2.js')}}"></script>
+
 
     <!-- Vendors JS -->
-    <script src="{{asset('backendvuexy/assets/vendor/libs/apex-charts/apexcharts.js')}}"></script>
-    <script src="{{asset('backendvuexy/assets/vendor/libs/swiper/swiper.js')}}"></script>
-    <script src="{{asset('backendvuexy/assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js')}}"></script>
+    <script src="{{asset('assets/vendor/libs/apex-charts/apexcharts.js')}}"></script>
+    <script src="{{asset('assets/vendor/libs/swiper/swiper.js')}}"></script>
+    <script src="{{asset('assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js')}}"></script>
 
 
 <!-- Main JS -->
