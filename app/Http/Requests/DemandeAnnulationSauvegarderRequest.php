fb27e57ddcb03c2c846b9d1ba11ed2860736c36b
Hermann Dorgeles <yancho@ldfgroupe.com>
2024-01-02 08:20:58 +0000

    ajout du request pour la demande d'annulation de plan

--- app/Http/Requests/DemandeAnnulationSauvegarderRequest.php
+++ app/Http/Requests/DemandeAnnulationSauvegarderRequest.php
@@ -19,6 +19,7 @@ class DemandeAnnulationSauvegarderRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
+
     public function rules(): array
     {
         if($this->id_demande){
