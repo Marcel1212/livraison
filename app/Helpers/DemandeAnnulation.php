af62ff7bf943ab89debc8b2cf645a9aae1d44d0c
N'CHO YAPI HERMANN DORGELES <yancho@ldfgroupe.com>
2024-01-10 11:36:31 +0000

    bug fix demande annulation

--- app/Helpers/DemandeAnnulation.php
+++ app/Helpers/DemandeAnnulation.php
@@ -3,7 +3,7 @@
 
 use App\Models\DemandeAnnulationPlan;
 
-class DemandeAnnualtion
+class DemandeAnnulation
 {
     public static function get_demande_annulation_en_traitement(){
         $demande_annulation = DemandeAnnulationPlan::where('flag_demande_annulation_plan_valider_par_processus','<>',true)->get();
