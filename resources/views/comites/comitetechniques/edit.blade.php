29547fe806b053ae8a60693b67892275186b9ece
traore <motraore@ldfgroupe.com>
2024-04-08 17:13:39 +0000

    maj coorectif second

--- resources/views/comites/comitetechniques/edit.blade.php
+++ resources/views/comites/comitetechniques/edit.blade.php
@@ -250,7 +250,7 @@ if(!empty($anneexercice->date_prolongation_periode_exercice)){
                             style="margin-top: 13px !important">
                                 <thead>
                                     <tr>
-                                        <th><label>Selectionnez tous</label><br/><input type="checkbox" id="allcb" name="allcb"/></th>
+                                        <th><label>Cocher tout</label><br/><input type="checkbox" id="allcb" name="allcb"/></th>
                                         <th>Type processus </th>
                                         <th>Entreprise </th>
                                         <th>Conseiller </th>
